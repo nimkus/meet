59d9b29786cbfc1860d247087ce517c2af7ac54c
nimkus <164895942+nimkus@users.noreply.github.com>
2025-01-09 14:13:02 +0100

    implemented integration testing for Feature 1: Filter Events by City, Scenario 3 (task 4.5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from 'react';
-
-import EventList from './components/EventList';
+import React from 'react';
 import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { getEvents } from './api';
+import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api';
 
 import './App.css';
 
 const App = () => {
-  const [events, setEvents] = useState([]);
+  const [allLocations, setAllLocations] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
+  const [events, setEvents] = useState([]);
+  const [currentCity, setCurrentCity] = useState('See all cities');
 
   const fetchData = async () => {
     const allEvents = await getEvents();
-    setEvents(allEvents.slice(0, currentNOE));
+    const filteredEvents =
+      currentCity === 'See all cities' ? allEvents : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentCity]);
 
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
       <NumberOfEvents />
       <EventList events={events} />
     </div>
