1bc3e0bd616b1b27d26403379cdfb86e1ee7f798
nimkus <164895942+nimkus@users.noreply.github.com>
2025-01-17 17:21:02 +0100

    installed atatus

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import * as atatus from 'atatus-spa';
+atatus.config('27a67ad99644419c898c09d2d9cdc499').install();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
