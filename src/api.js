4dbaab61c6eb05b988d49aaf5711766aff79ed5e
nimkus <164895942+nimkus@users.noreply.github.com>
2025-01-21 19:01:31 +0100

    fix: update endpoints api.js

--- src/api.js
+++ src/api.js
@@ -62,12 +62,6 @@ export const getEvents = async () => {
     return mockData;
   }
 
-  if (!navigator.onLine) {
-    const events = localStorage.getItem('lastEvents');
-    NProgress.done();
-    return events ? JSON.parse(events) : [];
-  }
-
   const token = await getAccessToken();
 
   if (token) {
@@ -76,8 +70,6 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
-      localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
