dfd5071631e307b29e67651c9c6b3b179c65a409
nimkus <164895942+nimkus@users.noreply.github.com>
2025-01-23 15:54:27 +0100

    fix: uncessfully tried to fix errors when testing

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import PropTypes from 'prop-types';
 
 const CityEventsChart = ({ allLocations, events }) => {
   const [data, setData] = useState([]);
 
-  const getData = () => {
-    const data = allLocations.map((location) => {
+  const getData = useCallback(() => {
+    return allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length;
       const city = location.split(/, | - /)[0];
       return { city, count };
     });
-    return data;
-  };
+  }, [allLocations, events]);
 
   useEffect(() => {
     setData(getData());
-  }, [`${events}`, `${data}`]);
+  }, [getData]);
 
   return (
     <ResponsiveContainer width="99%" height={400}>
@@ -42,6 +42,19 @@ const CityEventsChart = ({ allLocations, events }) => {
       </ScatterChart>
     </ResponsiveContainer>
   );
+};
+
+CityEventsChart.propTypes = {
+  events: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      summary: PropTypes.string.isRequired,
+      location: PropTypes.string,
+      created: PropTypes.string.isRequired,
+      description: PropTypes.string,
+    })
+  ).isRequired,
+  allLocations: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default CityEventsChart;
